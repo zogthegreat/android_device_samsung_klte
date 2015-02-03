3c68de945fa2f622f86dd4995cb42a52e7a4dc29
martincz <martincz@mokeedev.com>
2015-02-03 17:09:44 +0800

    klte: mokee bringup

---
+++ mokee.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
+
+$(call inherit-product, device/samsung/klte/full_klte.mk)
+
+PRODUCT_DEVICE := klte
+PRODUCT_NAME := mk_klte
